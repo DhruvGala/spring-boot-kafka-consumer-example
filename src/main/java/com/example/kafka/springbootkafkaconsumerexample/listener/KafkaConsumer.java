bbe7cd35745064c3bd119663a9afa7a084672175
Dhruv Gala <dhruv.g123@gmail.com>
2022-03-10 15:31:59 -0900

    listener/consumer added

---
+++ src/main/java/com/example/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package com.example.kafka.springbootkafkaconsumerexample.listener;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("consumed: " + message);
+    }
+}
